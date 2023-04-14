09ac93d6fee825104aea961c2ccf890e452ba96e
P0se1d0N <50700745+OlehKozii@users.noreply.github.com>
2023-04-14 22:17:09 +0300

    Update openai.js

--- openai.js
+++ openai.js
@@ -1,6 +1,6 @@
 const { OpenAIApi, Configuration } = require("openai");
 
-const model = "gpt-4";
+const model = "gpt-3.5-turbo";
 
 const config = new Configuration({
   apiKey: process.env.openaiToken,
