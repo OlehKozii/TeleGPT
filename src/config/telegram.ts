9724f8177cbc70f0ad99279dff43b3fd1d4b4d19
P0se1d0N <50700745+OlehKozii@users.noreply.github.com>
2023-04-18 02:03:16 +0200

    Disabled polling

--- src/config/telegram.ts
+++ src/config/telegram.ts
@@ -1,5 +1,5 @@
 import TelegramBot from "node-telegram-bot-api";
 
-const bot = new TelegramBot(`${process.env.telegramToken}`, { polling: true });
+const bot = new TelegramBot(`${process.env.telegramToken}`, { polling:false });
 
 export default bot;
